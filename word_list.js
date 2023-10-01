f259fc26c06b00ecb72191213f6e2fd8f8380b75
sihyun10 <hyhy200170@gmail.com>
2023-10-02 01:27:16 +0900

    feat: 영어단어 등록하기 제약사항에 '랜덤으로 띄워주기 위해 영어단어 30개 이상 등록되어있어야한다'라고 되어있었기에 영어단어들을 등록해두었다. (주의점은 단어의 글자수가 10개 이상을 넘어가지않게 등록하였다)

---
+++ word_list.js
@@ -0,0 +1,105 @@
+// 30개 이상 등록, 영어단어 글자 수 최대 10개
+const wordList = [
+  {
+    word: 'hanghae'
+  },
+  {
+    word: 'price'
+  },
+  {
+    word: 'schedule'
+  },
+  {
+    word: 'gift'
+  },
+  {
+    word: 'nurse'
+  },
+  {
+    word: 'experience'
+  },
+  {
+    word: 'comfort'
+  },
+  {
+    word: 'education'
+  },
+  {
+    word: 'object'
+  },
+  {
+    word: 'system'
+  },
+  {
+    word: 'respect'
+  },
+  {
+    word: 'effort'
+  },
+  {
+    word: 'glory'
+  },
+  {
+    word: 'interest'
+  },
+  {
+    word: 'junior'
+  },
+  {
+    word: 'practice'
+  },
+  {
+    word: 'balance'
+  },
+  {
+    word: 'stress'
+  },
+  {
+    word: 'wolf'
+  },
+  {
+    word: 'god'
+  },
+  {
+    word: 'ceiling'
+  },
+  {
+    word: 'business'
+  },
+  {
+    word: 'chance'
+  },
+  {
+    word: 'voice'
+  },
+  {
+    word: 'puzzle'
+  },
+  {
+    word: 'problem'
+  },
+  {
+    word: 'piece'
+  },
+  {
+    word: 'weather'
+  },
+  {
+    word: 'hole'
+  },
+  {
+    word: 'developer'
+  },
+  {
+    word: 'umbrella'
+  },
+  {
+    word: 'handshake'
+  },
+  {
+    word: 'towel'
+  },
+  {
+    word: 'bike'
+  },
+];
